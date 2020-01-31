12bf94d21cf9b25ff556e8aefc208989b391c55f
Mihir Dilip <mihir_dilip@hotmail.co.uk>
2020-01-31 10:06:41 +0000

    Windows Authentication Added

---
+++ src/Authentication.ClientSide.Windows/Program.cs
@@ -0,0 +1,22 @@
+﻿using RestApi.Client;
+using RestApi.Client.Authentication;
+using System;
+using System.Threading.Tasks;
+
+namespace Authentication.ClientSide
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var client = new RestClientBuilder()
+				.SetBaseAddress(new Uri(Constants.BaseUrl))
+				.AddWindowsDefaultAuthentication()
+				.Build();
+
+			await client.ExecuteConsoleTestRequestAsync().ConfigureAwait(false);
+
+			Console.ReadKey();
+		}
+	}
+}
